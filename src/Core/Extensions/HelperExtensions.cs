a7e8426bba837794798f6f5f15e909fa61cfd7ae
Stefano <schmidtsigma@gmail.com>
2025-03-16 18:13:05 +0300

    add TradingPair enum normalizer

---
+++ src/Core/Extensions/HelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CryptoTracker.Core.Enums;
+
+namespace CryptoTracker.Core.Extensions;
+
+internal static class HelperExtensions
+{
+	// Note: While Binance, Bitget and Bybit accept pairs in a combined form (e.g., BTCUSDT),
+	// Kucoin requires the pair to be separated by a hyphen ('-') (e.g., BTC-USDT).
+	internal static string ToNormalizedString(this TradingPair pair, char? separator = null)
+		=> pair.ToString().Replace("_", separator?.ToString() ?? string.Empty);
+	
+}
