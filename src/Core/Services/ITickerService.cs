fd1fc59bef64270979dd38424e077af9bb5405c6
Stefano <schmidtsigma@gmail.com>
2025-03-16 19:49:10 +0300

    define ITickerService interface

---
+++ src/Core/Services/ITickerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.Objects.Sockets;
+using CryptoTracker.Core.Entities;
+using CryptoTracker.Core.Enums;
+
+namespace CryptoTracker.Core.Services;
+
+public interface ITickerService
+{
+	/// <summary>
+	/// The name of the exchange being used.
+	/// </summary>
+	string Exchange { get; }
+	
+	Task<CallResult<TickerData>> GetTickerAsync(TradingPair pair, CancellationToken ct = default);
+	
+	Task<CallResult<UpdateSubscription>> SubscribeToTickerUpdatesAsync(
+		TradingPair pair, Action<TickerData> onUpdate, CancellationToken ct = default);
+}
