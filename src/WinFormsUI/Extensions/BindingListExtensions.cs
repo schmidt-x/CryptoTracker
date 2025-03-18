2d968c308f52ed6483b6a3da0923207784a31887
Stefano <schmidtsigma@gmail.com>
2025-03-18 23:01:22 +0300

    add extension method for BindingList<TickerModel>

---
+++ src/WinFormsUI/Extensions/BindingListExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using CryptoExchange.Net;
+using CryptoTracker.Core.Entities;
+using CryptoTracker.WinFormsUI.Models;
+
+namespace CryptoTracker.WinFormsUI.Extensions;
+
+internal static class BindingListExtensions
+{
+	internal static void UpdateModel(this BindingList<TickerModel> models, TickerData ticker)
+	{
+		var model = models.First(m => m.Exchange.Equals(ticker.Exchange, StringComparison.Ordinal));
+		var positiveChange = ticker.ChangePercentage24H > 0 ? "+" : "";
+		
+		model.Price = ticker.LastPrice.Normalize();
+		model.Low24H = ticker.LowPrice24H.Normalize();
+		model.High24H = ticker.HighPrice24H.Normalize();
+		model.ChangePercentage24H = $"{positiveChange}{ticker.ChangePercentage24H.Normalize()}%";
+	}
+}
