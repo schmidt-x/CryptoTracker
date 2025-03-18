76b012a93196266be0e4c51a12d3b850f871d5d1
Stefano <schmidtsigma@gmail.com>
2025-03-18 19:32:35 +0300

    add ugly logger

---
+++ src/WinFormsUI/Common/ILogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CryptoTracker.WinFormsUI.Common;
+
+public interface ILogger
+{
+	void LogMessage(string message);
+}
+
+public class DummyLogger : ILogger
+{
+	private readonly ListBox _listBox;
+
+	public DummyLogger(ListBox listBox)
+	{
+		_listBox = listBox;
+	}
+	
+	public void LogMessage(string message)
+	{
+		if (_listBox.InvokeRequired)
+		{
+			_listBox.Invoke(Log, message);
+		}
+		else
+		{
+			Log(message);
+		}
+	}
+	
+	private void Log(string message)
+	{
+		var msg = $"{DateTimeOffset.UtcNow:O} {message}";
+		
+		_listBox.Items.Add(msg);
+		_listBox.SelectedIndex = _listBox.Items.Count - 1;
+	}
+}
